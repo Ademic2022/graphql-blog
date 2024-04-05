8832107f62b9257a13a3f9d1acc510ed14c10942
ademic2022 <adeeyomichael77@gmail.com>
2024-04-05 10:16:38 +0100

    feat: post(createPost, updatePost) implemented

--- core/mutations.py
+++ core/mutations.py
@@ -1,8 +1,11 @@
 import graphene
 
 from categories.mutations import CreateCategory, UpdateCategory, DeleteCategory
+from posts.mutations import CreatePost, UpdatePost
 
 class Mutation(graphene.ObjectType):
     create_category = CreateCategory.Field()
     update_category = UpdateCategory.Field()
     delete_category = DeleteCategory.Field()
+    create_post = CreatePost.Field()
+    update_post = UpdatePost.Field()
